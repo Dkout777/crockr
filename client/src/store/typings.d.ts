4a34ea2631fd9576e4e9a1c3d5715cea533cef39
Dimitry Koutchine <72232945+Dkout777@users.noreply.github.com>
2022-02-07 21:41:50 -0500

    Delete client directory

--- client/src/store/typings.d.ts
+++
@@ -1,9 +0,0 @@
-import { roomState } from './modules/roomID';
-import { SocketState } from './modules/socket';
-
-export interface State {
-  roomID: roomState;
-  chat: ChatMessagesState
-  colourPalette: ColourPaletteState
-  socket: SocketState
-}
